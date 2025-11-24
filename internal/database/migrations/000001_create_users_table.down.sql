99ff73bc7fd5de72aad4f76108fc0bfb36826cc2
Andhika-GIT <andhikapr081@gmail.com>
2025-11-24 12:48:43 +0700

    remove unnecessary comments

--- internal/database/migrations/000001_create_users_table.down.sql
+++ internal/database/migrations/000001_create_users_table.down.sql
@@ -1,4 +1,4 @@
--- Drop indexes terlebih dahulu
+-- Drop indexes
 DROP INDEX IF EXISTS idx_users_deleted_at;
 DROP INDEX IF EXISTS idx_users_email_verified;
 DROP INDEX IF EXISTS idx_users_is_active;
